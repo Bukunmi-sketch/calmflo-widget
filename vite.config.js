cc3acb802b881fe6ae2d1f30e6c2e88b43615b0b
Bukunmi-sketch <olarindebukunmi@gmail.com>
2025-09-29 22:33:35 +0100

    vite config issue

--- vite.config.js
+++ vite.config.js
@@ -52,6 +52,40 @@
 // });
 
 
+// // vite.config.js
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
+// export default defineConfig(({ command }) => {
+//   if (command === "serve") {
+//     // DEV: run normal React app with index.html
+//     return {
+//       plugins: [react(), tailwindcss()],
+//     };
+//   } else {
+//     // PROD: build a single UMD bundle with CSS inlined
+//     return {
+//       plugins: [react(), tailwindcss(), cssInjectedByJsPlugin()],
+//       build: {
+//         lib: {
+//           entry: "src/widget-entry.jsx",
+//           name: "ChatWidget",
+//           fileName: "chat-widget",
+//           formats: ["umd"],
+//         },
+//         rollupOptions: {
+//           external: [],
+//           output: { globals: {} },
+//         },
+//         cssCodeSplit: false, // ⬅️ force bundling CSS into JS
+//         emptyOutDir: true,
+//       },
+//     };
+//   }
+// });
+
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
@@ -68,6 +102,9 @@ export default defineConfig(({ command }) => {
     // PROD: build a single UMD bundle with CSS inlined
     return {
       plugins: [react(), tailwindcss(), cssInjectedByJsPlugin()],
+      define: {
+        "process.env.NODE_ENV": JSON.stringify("production"), // 👈 add this
+      },
       build: {
         lib: {
           entry: "src/widget-entry.jsx",
